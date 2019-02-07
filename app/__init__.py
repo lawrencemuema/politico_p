b4e249129eef498046a61445bc6e52c0dfe88699
lawrencemuema <lawrencemuema96@gmail.com>
2019-02-07 21:21:14 +0300

    [feature #163782807]

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,33 @@
+""" Global Imports """
 from flask import Flask, Blueprint
+from flask_restful import Api
 
+""" Importing Blueprints """
+from .api.v1 import version_one as v1
 
-from .api.v1.views import version_one as v1
+""" local module imports """
+from .api.v1.views import Party,Office,AllOffices,AllParties
 
-
-
-
+""" creating an application instance """
 def start_app():
+   ## app = Flask(__name__, instance_relative_config=True)
+
+    #app.config.from_object(app_config[config_name])
+    #app.config.from_pyfile('config.py')
+
     app = Flask(__name__)
 
-    app.register_blueprint(v1)
+    """ Registering application blueprint for admin views"""
+    api = Api(v1)
+    app.register_blueprint(v1, url_prefix='/api/v1')
+
+    """ creating admin enpoints"""
+
+    api.add_resource(Party, '/party/<int:p_id>')
+    api.add_resource(AllParties, '/party')
+
+    api.add_resource(Office, '/office/<int:o_id>')
+    api.add_resource(AllOffices, '/office')
+
 
     return app
--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,16 +1,8 @@
 from .api.v1.views import Party,Office,AllOffices,AllParties
 from flask import Blueprint
-from flask_restful import Api, Resource
 
 
 
 #versioning
 version_one = Blueprint('api_v1', __name__, url_prefix = '/api/v1')
-api = Api(version_one)
 
-
-api.add_resource(Party, '/party/<int:p_id>')
-api.add_resource(AllParties, '/party')
-
-api.add_resource(Office, '/office/<int:o_id>')
-api.add_resource(AllOffices, '/office')
